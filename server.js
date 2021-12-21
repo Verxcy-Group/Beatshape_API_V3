7394cf80db9b84a1eca33c388ac85e4dbf9ebb5a
BeyYT <60990459+BeyYT@users.noreply.github.com>
2021-12-21 14:21:57 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,1 @@
+const express=require("express"),bodyParser=require("body-parser"),app=express(),fs=require("fs");var apiKey="";app.use(bodyParser.json()),app.use(bodyParser.urlencoded({extended:!0}));const routes=require("./routes/routes.js")(app,fs),server=app.listen(728,()=>{console.log("listening on port %s...",server.address().port)});
