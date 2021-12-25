1da6392f2262500f962dabf910445081f59362d3
BeyYT <60990459+BeyYT@users.noreply.github.com>
2021-12-25 12:34:56 -0700

    update 3.1

--- routes/main.js
+++ routes/main.js
@@ -1,61 +1,54 @@
-var util = require("util"),
-  serveIndex = require("serve-index");
+const util = require("util")
+const serveIndex = require("serve-index");
 const userRoutes = (app, fs) => {
-  let dataPath = "./data/maps.json",
-    datafol = "./data",
-    a = "<p>";
-  const readFile = (e, a = !1, r = dataPath, t = "utf8") => {
-      fs.readFile(r, t, (r, t) => {
-        if (r) throw r;
-        e(a ? JSON.parse(t) : t);
-      });
-    },
-    writeFile = (e, a, r = dataPath, t = "utf8") => {
-      fs.writeFile(r, e, t, e => {
-        if (e) throw e;
-        a();
-      });
-    };
-	app.get("/files/:id", (e, a) => {
-		(id = e.params.id),
-			fs.readFile("./data/" + id + ".map", "utf8", (e, r) => {
-				if (e) throw e;
-				a.send(JSON.parse(r)), console.log("hey someone checked their account");
+    let dataPath = "./data/maps.json",
+        datafol = "./data",
+        a = "<p>";
+    const readFile = (e, a = !1, r = dataPath, t = "utf8") => {
+            fs.readFile(r, t, (r, t) => {
+                if (r) throw r;
+                e(a ? JSON.parse(t) : t);
+            });
+        },
+        writeFile = (e, a, r = dataPath, t = "utf8") => {
+            fs.writeFile(r, e, t, (e) => {
+                if (e) throw e;
+                a();
+            });
+        };
+    app.get("/files/:id", (e, a) => {
+        (id = e.params.id),
+            fs.readFile("./data/s" + id + ".map", "utf8", (e, r) => {
+                if (e) throw e;
+                a.send(JSON.parse(r)), console.log("hey someone checked their account");
+            });
+    }),
+        app.get("/files", (e, r) => {
+            fs.readdirSync(datafol).forEach((e) => {
+                a = a + e + "</p><p>";
+            }),
+                r.send("<style>h{font-size:24px;color:gray}p{color:#008b8b;font-size:16px}body{background-color:#000;font-family:monospace}</style><h>files:</h> " + a),
+                (a = "<p>");
+        }),
+        app.put("/files/:id", (req, res) => {
+            (id = req.params.id),
+                (enewdat = req.body),
+                (newdat = JSON.stringify(enewdat)
+                    .replace(/\\/g, "")
+                    .replace(/","token":"/g, "&token=")
+                    .replace(/''/g, "")
+                    .replace(/}}}}"}/g, "}}}}}")
+                    .replace('"{', "{")),
+                eval("fs.writeFile('./data/" + id + ".map',newdat, function (err) { if (err) throw err; console.log('File is created successfully.')});");
+        }),
+        app.delete("/users/:id", (e, a) => {
+            readFile((r) => {
+                const t = e.params.id;
+                delete r[t],
+                    writeFile(JSON.stringify(r, null, 2), () => {
+                        a.status(200).send(`Map:${t} removed`);
+                    });
+            }, !0);
         });
-    }),
-    app.get("/files", (e, r) => {
-      fs.readdirSync(datafol).forEach(e => {
-        a = a + e + "</p><p>";
-      }),
-        r.send(
-          "<style>h{font-size:24px;color:gray}p{color:#008b8b;font-size:16px}body{background-color:#000;font-family:monospace}</style><h>files:</h> " +
-            a
-        ),
-        (a = "<p>");
-    }),
-    app.put("/files/:id", (req, res) => {
-      (id = req.params.id),
-        (enewdat = req.body),
-        (newdat = JSON.stringify(enewdat)
-          .replace(/\\/g, "")
-          .replace(/","token":"/g, "&token=")
-          .replace(/''/g, "")
-          .replace(/}}}}"}/g, "}}}}}")
-          .replace('"{', "{")),
-        eval(
-          "fs.writeFile('./data/" +
-            id +
-            ".map',newdat, function (err) { if (err) throw err; console.log('File is created successfully.')});"
-        );
-    }),
-    app.delete("/users/:id", (e, a) => {
-      readFile(r => {
-        const t = e.params.id;
-        delete r[t],
-          writeFile(JSON.stringify(r, null, 2), () => {
-            a.status(200).send(`Map:${t} removed`);
-          });
-      }, !0);
-    });
 };
 module.exports = userRoutes;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express"),
-  bodyParser = require("body-parser"),
-  app = express(),
-  fs = require("fs");
+    bodyParser = require("body-parser"),
+    app = express(),
+    fs = require("fs");
 var apiKey = "";
 app.use(bodyParser.json()), app.use(bodyParser.urlencoded({ extended: !0 }));
+const { PeerServer } = require('peer');
 const routes = require("./routes/routes.js")(app, fs),
-  server = app.listen(process.env.PORT || 728, () => {
-    console.log("listening on port %s...", server.address().port);
-  });
+    server = app.listen(728, () => {
+        console.log("listening on port %s...", server.address().port);
+        const ps1 = PeerServer({ port: 100, path: '/spec' });
+        const ps2 = PeerServer({ port: 200, path: '/multi' });
+        const ps3 = PeerServer({ port: 300, path: '/chat' });
+    });
